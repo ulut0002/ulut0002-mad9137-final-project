7377564435cdccf7e9e6a64eabe4b88c242c71b9
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-11-22 21:21:50 -0500

    Initial Commit

---
+++ OttawaOpenDoors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OttawaOpenDoors
+//
+//  Created by Serdar Ulutas on 2023-11-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OttawaOpenDoors/OttawaOpenDoorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OttawaOpenDoorsApp.swift
+//  OttawaOpenDoors
+//
+//  Created by Serdar Ulutas on 2023-11-22.
+//
+
+import SwiftUI
+
+@main
+struct OttawaOpenDoorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
